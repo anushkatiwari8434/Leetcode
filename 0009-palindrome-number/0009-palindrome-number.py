e113d6d5c3e94d7a5cb71b4ca61c103bf9f51a89
anushkatiwari8434 <anushkatiwari8434@gmail.com>
2026-08-03 23:21:54 +0530

    Time: 9 ms (45.1%), Space: 19.2 MB (54.05%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+
+        rev=0
+        original=x
+
+        while x>0:
+            digit=x%10
+            rev=rev*10+digit
+            x//=10
+
+        return rev==original
+        
