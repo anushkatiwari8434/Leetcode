c605e17340ff77d9fa158d12c1c91147238bb06f
anushkatiwari8434 <anushkatiwari8434@gmail.com>
2026-08-27 12:13:50 +0530

    Time: 0 ms (100%), Space: 19.2 MB (50.1%) - LeetHub

---
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.py
@@ -0,0 +1,7 @@
+class Solution:
+    def checkIfPangram(self, sentence: str) -> bool:
+        for ch in "abcdefghijklmnopqrstuvwxyz":
+            if ch not in sentence:
+                return False
+
+        return True
